961024fb604bbb5f73c7886d1d438d5cf78cac05
Rebecca Tong <rtong@buffalo.edu>
2018-05-10 17:06:32 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,22 @@ class Show < ActiveRecord::Base
     Show.maximum(:rating)
   end
 
+  def self.most_popular_show
+  end
+
+  def self.lowest_rating
+  end
+
+  def self.least_popular_show
+  end
+
+  def self.ratings_sum
+  end
+
+  def self.popular_shows
+  end
+
+  def shows_by_alphabetical_order
+  end
+
 end
